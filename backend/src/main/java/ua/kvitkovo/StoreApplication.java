6390190e4671cc03d055f20d6c5a779983fc0831
gaponov <wmconstructor@gmail.com>
2023-07-23 21:23:44 +0300

    initial commit

---
+++ backend/src/main/java/ua/kvitkovo/StoreApplication.java
@@ -0,0 +1,15 @@
+package ua.kvitkovo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Andriy Gaponov
+ */
+@SpringBootApplication
+public class StoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StoreApplication.class, args);
+    }
+}
