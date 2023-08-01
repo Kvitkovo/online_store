cf07964122217032f2588a9beb4228a1767bd081
annaBalabak <annabalabak15@gmail.com>
2023-08-01 16:13:34 +0200

    added React Router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,32 @@
+import React from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import Promotions from './pages/Promotions'
+import About from './pages/About'
+import Shops from './pages/Shops'
+import Delivery from './pages/Delivery'
+import Handling from './pages/Handling'
 import './App.scss'
+import Header from './components/Header'
 
 function App() {
-  return <div className="App">Test</div>
+  return (
+    <Router>
+      <div className="App">
+        <Header />
+        <div>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/promotions" element={<Promotions />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/shops" element={<Shops />} />
+            <Route path="/delivery" element={<Delivery />} />
+            <Route path="/handling" element={<Handling />} />
+          </Routes>
+        </div>
+      </div>
+    </Router>
+  )
 }
 
 export default App
---
+++ frontend/src/pages/Promotions.js
@@ -0,0 +1,8 @@
+function Promotions() {
+    return (
+      <div>
+        <h1>Promotions</h1>
+      </div>
+    )
+}
+export default Promotions
