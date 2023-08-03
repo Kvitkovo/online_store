2acf5ca58ed662bd3474fe80e40c6760b4c484e0
Svitlana-G <svitlana.golubieva@gmail.com>
2023-08-03 14:31:22 +0200

    Add funtions for carousel

--- frontend/src/components/Carousel.jsx
+++ frontend/src/components/Carousel.jsx
@@ -1,4 +1,5 @@
 import '../scss/Carousel.scss'
+import { useState } from 'react'
 
 const Carousel = () => {
   const data = [
@@ -6,27 +7,49 @@ const Carousel = () => {
     { src: './images/banner-02.jpg', alt: 'другий банер' },
     { src: './images/banner-03.jpg', alt: 'третій банер' },
   ]
+
+  const [slide, setSlide] = useState(0)
+
+  const nextSlide = () => {
+    setSlide(slide === data.length - 1 ? 0 : slide + 1)
+  }
+  const prevSlide = () => {
+    setSlide(slide === 0 ? data.length - 1 : slide - 1)
+  }
   return (
     <div className="carousel">
       <img
         src="http://localhost:3000/images/button_left.jpg"
         alt="стрілка ліворуч"
         className="arrow arrow-left"
+        onClick={prevSlide}
       ></img>
       {data.map((item, index) => {
         return (
-          <img src={item.src} alt={item.alt} key={index} className="slide" />
+          <img
+            src={item.src}
+            alt={item.alt}
+            key={index}
+            className={slide === index ? 'slide' : 'slide slide-hidden'}
+          />
         )
       })}
       <img
         src="./images/button_right.jpg"
         alt="стрілка праворуч"
         className="arrow arrow-right"
+        onClick={nextSlide}
       ></img>
       <span className="indicators">
-        {data.map((index) => {
+        {data.map((item, index) => {
           return (
-            <button key={index} onClick={null} className="indicator"></button>
+            <button
+              key={index}
+              onClick={() => setSlide(index)}
+              className={
+                slide === index ? 'indicator' : 'indicator indicator-inactive'
+              }
+            ></button>
           )
         })}
       </span>
