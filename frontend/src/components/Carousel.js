0586c6f31ee238955ece97364af1de62b062ca64
Svitlana-G <svitlana.golubieva@gmail.com>
2023-08-07 16:32:32 +0200

    Add left and right swipe for mobile

--- frontend/src/components/Carousel.js
+++ frontend/src/components/Carousel.js
@@ -9,6 +9,8 @@ const Carousel = () => {
   ];
 
   const [slide, setSlide] = useState(0);
+  const [touchStart, setTouchStart] = useState(null);
+  const [touchEnd, setTouchEnd] = useState(null);
 
   useEffect(() => {
     let slider = setInterval(
@@ -31,8 +33,34 @@ const Carousel = () => {
     setSlide(slide === 0 ? data.length - 1 : slide - 1);
   };
 
+  // the required distance between touchStart and touchEnd to be detected as
+  //  a swipe
+  const minSwipeDistance = 50;
+
+  const onTouchStart = (e) => {
+    // otherwise the swipe is fired even with usual touch events
+    setTouchEnd(null);
+    setTouchStart(e.targetTouches[0].clientX);
+  };
+
+  const onTouchMove = (e) => setTouchEnd(e.targetTouches[0].clientX);
+
+  const onTouchEnd = () => {
+    if (!touchStart || !touchEnd) return;
+    const distance = touchStart - touchEnd;
+    const isLeftSwipe = distance > minSwipeDistance;
+    const isRightSwipe = distance < -minSwipeDistance;
+    if (isLeftSwipe || isRightSwipe) {
+      isLeftSwipe ? prevSlide() : nextSlide();
+    }
+  };
   return (
-    <div className="carousel">
+    <div
+      className="carousel"
+      onTouchStart={onTouchStart}
+      onTouchMove={onTouchMove}
+      onTouchEnd={onTouchEnd}
+    >
       <img
         src="http://localhost:3000/images/button_left.jpg"
         alt="стрілка ліворуч"
