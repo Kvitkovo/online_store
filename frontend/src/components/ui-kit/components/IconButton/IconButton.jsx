2ef667cb1f50a557c50180159ca14f030624620e
Svitlana-G <svitlana.golubieva@gmail.com>
2023-08-22 14:40:23 +0200

    Delete enter

--- frontend/src/components/ui-kit/components/IconButton/IconButton.jsx
+++ frontend/src/components/ui-kit/components/IconButton/IconButton.jsx
@@ -4,18 +4,16 @@ import styles from './IconButton.module.scss';
 const IconButton = (props) => {
   return (
     <button
-      className={`${
+      className={`${styles.button} ${
         props.isBorder ? `${styles['icon-border']}` : `${styles.icon}`
-      }
-        ${props.isRound && `${styles['icon-round']}`}
-        ${props.isOpacity && `${styles['icon-opacity']}`}
-        ${props.isRoundWithPadding && `${styles['icon-round-padding']}`}
-        ${props.isBackground && `${styles['icon-background']}`}
-        ${
-          props.isRectangularWithPadding &&
-          `${styles['icon-rectangular-padding']}`
-        }
-        ${props.isRoundGreen && `${styles['icon-round-green']}`}
+      } ${props.isRound && `${styles['icon-round']}`} ${
+        props.isOpacity && `${styles['icon-opacity']}`
+      } ${props.isRoundWithPadding && `${styles['icon-round-padding']}`} ${
+        props.isBackground && `${styles['icon-background']}`
+      } ${
+        props.isRectangularWithPadding &&
+        `${styles['icon-rectangular-padding']}`
+      } ${props.isRoundGreen && `${styles['icon-round-green']}`}
         `}
       onClick={onclick}
     >
