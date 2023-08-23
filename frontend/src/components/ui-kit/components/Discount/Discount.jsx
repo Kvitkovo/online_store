0a57be72379265bc8ccebf169434c2af81d0d8c3
Svitlana-G <svitlana.golubieva@gmail.com>
2023-08-23 16:15:05 +0200

    Change props

--- frontend/src/components/ui-kit/components/Discount/Discount.jsx
+++ frontend/src/components/ui-kit/components/Discount/Discount.jsx
@@ -1,15 +1,15 @@
 import React from 'react';
 import styles from './Discount.module.scss';
 
-const Discount = (props) => {
+const Discount = ({ discount, isBigCard = false }) => {
   return (
-    <div>
-      {props.isSmallCard ? (
-        <div className={styles.discount}>-{props.discount}%</div>
+    <>
+      {isBigCard ? (
+        <div className={styles.bigCardDiscount}>-{discount}% знижка</div>
       ) : (
-        <div className={styles.bigCardDiscount}>-{props.discount}% знижка</div>
+        <div className={styles.discount}>-{discount}%</div>
       )}
-    </div>
+    </>
   );
 };
 
--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -7,8 +7,8 @@ const Home = () => {
   return (
     <div className="home">
       <Carousel />
-      <Discount discount={15} isSmallCard={true} />
-      <Discount discount={15} isSmallCard={false} />
+      <Discount discount={15} />
+      <Discount discount={15} isBigCard={true} />
     </div>
   );
 };
