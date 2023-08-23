35e1130393547eaadb896939421bf639227fda1b
Svitlana-G <svitlana.golubieva@gmail.com>
2023-08-23 13:56:32 +0200

    Add styles for big card

--- frontend/src/components/ui-kit/components/Discount/Discount.jsx
+++ frontend/src/components/ui-kit/components/Discount/Discount.jsx
@@ -2,7 +2,15 @@ import React from 'react';
 import styles from './Discount.module.scss';
 
 const Discount = (props) => {
-  return <div className={styles.discount}>{props.discount}%</div>;
+  return (
+    <div>
+      {props.isSmallCard ? (
+        <div className={styles.discount}>-{props.discount}%</div>
+      ) : (
+        <div className={styles.bigCardDiscount}>-{props.discount}% знижка</div>
+      )}
+    </div>
+  );
 };
 
 export default Discount;
--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -7,7 +7,8 @@ const Home = () => {
   return (
     <div className="home">
       <Carousel />
-      <Discount discount={15} />
+      <Discount discount={15} isSmallCard={true} />
+      <Discount discount={15} isSmallCard={false} />
     </div>
   );
 };
