4ff4257a242ff9c00eda6a1aaf686b32410d46a0
annaBalabak <annabalabak15@gmail.com>
2023-09-01 12:36:20 +0200

    added useMemo

--- frontend/src/components/ui-kit/components/Button/Button.jsx
+++ frontend/src/components/ui-kit/components/Button/Button.jsx
@@ -1,11 +1,14 @@
-import React, { memo } from 'react';
+import React, { memo, useMemo } from 'react';
 import styles from './Button.module.scss';
 
 const Button = memo(
   ({ label, variant = 'primary', padding, type = 'button', icon, onClick }) => {
-    const buttonClasses = `${styles.button} ${styles[variant]} ${
-      padding && styles[padding]
-    }`;
+    const buttonClasses = useMemo(() => {
+      return `${styles.button} ${styles[variant]} ${
+        padding && styles[padding]
+      }`;
+    }, [variant, padding]);
+
     return (
       <button className={buttonClasses} type={type} onClick={onClick}>
         <span className={styles.label}>{label}</span>
