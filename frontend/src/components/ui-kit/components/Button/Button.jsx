ef591142a57aff1dc0643b04f82c5087db23b3cf
annaBalabak <annabalabak15@gmail.com>
2023-09-02 18:59:36 +0200

    corrected padding classes

--- frontend/src/components/ui-kit/components/Button/Button.jsx
+++ frontend/src/components/ui-kit/components/Button/Button.jsx
@@ -4,11 +4,18 @@ import styles from './Button.module.scss';
 const Button = memo(
   ({ label, variant = 'primary', padding, type = 'button', icon, onClick }) => {
     const buttonClasses = useMemo(() => {
-      return `${styles.button} ${styles[variant]} ${
-        padding && styles[padding]
-      }`;
+      const classes = [styles.button, styles[variant]];
+      if (padding === 'btn-catalogue') {
+        classes.push(styles['btn-catalogue']);
+      }
+      if (padding === 'btn-add') {
+        classes.push(styles['btn-add']);
+      }
+      if (padding === 'btn-added') {
+        classes.push(styles['btn-added']);
+      }
+      return classes.join(' ');
     }, [variant, padding]);
-
     return (
       <button className={buttonClasses} type={type} onClick={onClick}>
         <span className={styles.label}>{label}</span>
