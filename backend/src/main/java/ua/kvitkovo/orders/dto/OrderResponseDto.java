8bc4d89b384976ccfc766cddef25d30c153241ed
gaponov <wmconstructor@gmail.com>
2023-09-03 21:13:42 +0300

    add empty order dtos

---
+++ backend/src/main/java/ua/kvitkovo/orders/dto/OrderResponseDto.java
@@ -0,0 +1,17 @@
+package ua.kvitkovo.orders.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+/**
+ * @author Andriy Gaponov
+ */
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class OrderResponseDto {
+
+}
