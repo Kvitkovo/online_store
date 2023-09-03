4bf8bae4a8d6d63f18e0f3056423b99247ea80c1
hrechanyktolia <tolikhrehanyk25@gmail.com>
2023-09-04 00:41:12 +0300

    create wrraper

--- frontend/src/pages/Shops.js
+++ frontend/src/pages/Shops.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import Wrapper from '../components/Wrraper';
 
 function Shops() {
   return (
     <div>
       <h1>Shops</h1>
+      <Wrapper>
+        <h2>hello</h2>
+      </Wrapper>
     </div>
   );
 }
