155e83e014bd49802f6cafa0853fa8f4e85279ef
gaponov <wmconstructor@gmail.com>
2023-09-03 20:22:24 +0300

    add order item entity

---
+++ backend/src/main/java/ua/kvitkovo/orders/entity/OrderItem.java
@@ -0,0 +1,49 @@
+package ua.kvitkovo.orders.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import java.math.BigDecimal;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import ua.kvitkovo.catalog.entity.Product;
+
+/**
+ * @author Andriy Gaponov
+ */
+@Entity
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "orders")
+public class OrderItem {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "order_id", nullable = false)
+    private Order order;
+
+    @ManyToOne
+    @JoinColumn(name = "product_id", nullable = false)
+    private Product product;
+
+    @Column(name = "product_price")
+    private BigDecimal price;
+
+    @Column(name = "product_qty")
+    private BigDecimal qty;
+}
