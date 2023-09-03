fa8d03d3bb12c8d1130d98a75466855a86b79bb2
gaponov <wmconstructor@gmail.com>
2023-09-03 21:18:57 +0300

    add order repository and service

---
+++ backend/src/main/java/ua/kvitkovo/orders/service/OrderService.java
@@ -0,0 +1,15 @@
+package ua.kvitkovo.orders.service;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+/**
+ * @author Andriy Gaponov
+ */
+@Slf4j
+@RequiredArgsConstructor
+@Service
+public class OrderService {
+
+}
