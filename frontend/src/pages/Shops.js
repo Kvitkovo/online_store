e479d323414148a331ca1ee17d5564660454c493
hrechanyktolia <tolikhrehanyk25@gmail.com>
2023-09-04 20:17:49 +0300

    fix styles

--- frontend/src/pages/Shops.js
+++ frontend/src/pages/Shops.js
@@ -1,13 +1,9 @@
 import React from 'react';
-import Wrapper from '../components/Wrraper';
 
 function Shops() {
   return (
     <div>
       <h1>Shops</h1>
-      <Wrapper>
-        <h2>hello</h2>
-      </Wrapper>
     </div>
   );
 }
