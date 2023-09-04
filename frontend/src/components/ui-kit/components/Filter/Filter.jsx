ab0b1b660a166fcd21e69fb13c019ef141202a2a
annaBalabak <annabalabak15@gmail.com>
2023-09-04 10:31:13 +0200

    corrected icon

--- frontend/src/components/ui-kit/components/Filter/Filter.jsx
+++ frontend/src/components/ui-kit/components/Filter/Filter.jsx
@@ -7,9 +7,10 @@ const Filter = memo(({ label, onClick }) => {
   return (
     <div className={styles.filter}>
       <div className={styles.label}>{label}</div>{' '}
-      <div className={styles.icon}>
-        <IconButton icon={<ICONS.CloseIcon />} onClick={onClick} />
-      </div>
+      <IconButton
+        icon={<ICONS.CloseIcon className={styles.icon} />}
+        onClick={onClick}
+      />
     </div>
   );
 });
