a31ef188f42c8075619902f0f7838fcf970202a1
Svitlana-G <svitlana.golubieva@gmail.com>
2023-09-08 17:43:15 +0200

    Change Card.scss to Card.module.scss

--- frontend/src/components/Card/Card.jsx
+++ frontend/src/components/Card/Card.jsx
@@ -1,28 +1,53 @@
-import './Card.scss';
 import React from 'react';
+import styles from './Card.module.scss';
+
 const Card = (props) => {
   return (
-    <div className={props.status === 'NO_ACTIVE' ? 'card inactive' : 'card'}>
+    <div
+      className={
+        props.status === 'NO_ACTIVE'
+          ? `${styles.card + ' ' + styles.inactive}`
+          : `${styles.card}`
+      }
+    >
       <img src={props.image} alt="букет" />
       <h3>{props.title}</h3>
-      <div className={props.discount === 0 ? 'hide' : 'discount'}>
+      <div
+        className={
+          props.discount === 0 ? `${styles.hide}` : `${styles.discount}`
+        }
+      >
         {props.discount}%
       </div>
-      <div className="card-info">
+      <div className={styles['card-info']}>
         <p>Ціна</p>
-        <div className={props.discount === 0 ? 'hide-price' : 'old-price'}>
+        <div
+          className={
+            props.discount === 0
+              ? `${styles['hide-price']}`
+              : `${styles['old-price']}`
+          }
+        >
           {props.oldPrice} грн
         </div>
-        <div className="card-flex-bottom">
-          <div className="actual-price">
+        <div className={styles['card-flex-bottom']}>
+          <div className={styles['actual-price']}>
             {props.price}
             <span>грн</span>
           </div>
-          <div className={props.status === 'NO_ACTIVE' ? 'hide' : 'icons'}>
+          <div
+            className={
+              props.status === 'NO_ACTIVE'
+                ? `${styles.hide}`
+                : `${styles.icons}`
+            }
+          >
             <img
               src="./images/bouquet-card.svg"
               alt="додати в букет"
-              className={props.bouquet === false ? 'hide' : 'bouquet'}
+              className={
+                props.bouquet === false ? `${styles.hide}` : `${styles.bouquet}`
+              }
             />
             <img
               src="./images/cart-card.svg"
