8db088c5353e64617b26c1e19bbb199345baf04c
annaBalabak <annabalabak15@gmail.com>
2023-09-11 13:16:44 +0200

    created router.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,25 +3,24 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Promotions from './pages/Promotions';
 import About from './pages/About';
-import Shops from './pages/Shops';
 import Delivery from './pages/Delivery';
 import Handling from './pages/Handling';
 import './App.scss';
-// import Header from './components/Header';
+import ROUTES from './components/constants/routers';
+import Header from './components/Header';
 
 function App() {
   return (
     <Router>
       <div className="App">
-        {/* <Header /> */}
+        <Header />
         <div>
           <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/promotions" element={<Promotions />} />
-            <Route path="/about" element={<About />} />
-            <Route path="/shops" element={<Shops />} />
-            <Route path="/delivery" element={<Delivery />} />
-            <Route path="/handling" element={<Handling />} />
+            <Route path={ROUTES.home} element={<Home />} />
+            <Route path={ROUTES.promotions} element={<Promotions />} />
+            <Route path={ROUTES.about} element={<About />} />
+            <Route path={ROUTES.delivery} element={<Delivery />} />
+            <Route path={ROUTES.handling} element={<Handling />} />
           </Routes>
         </div>
       </div>
---
+++ frontend/src/components/constants/routers.js
@@ -0,0 +1,9 @@
+const ROUTES = {
+  home: '/',
+  promotions: '/promotions',
+  about: '/about',
+  delivery: '/delivery',
+  handling: '/handling',
+};
+
+export default ROUTES;
