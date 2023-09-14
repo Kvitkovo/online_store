68498962f46e8575b7d352e7d238c6ce0e6e5dd5
Svitlana-G <svitlana.golubieva@gmail.com>
2023-09-14 22:08:00 +0200

    Add className for currency

--- frontend/src/components/ui-kit/components/DiscountPrice/DiscountPrice.jsx
+++ frontend/src/components/ui-kit/components/DiscountPrice/DiscountPrice.jsx
@@ -3,13 +3,13 @@ import styles from './DiscountPrice.module.scss';
 
 const DiscountPrice = ({ oldPrice, actualPrice }) => {
   return (
-    <>
+    <div>
       <div className={styles['old-price']}>{oldPrice} грн</div>
       <div className={styles['actual-price']}>
         {actualPrice}
-        <span>грн</span>
+        <span className={styles.currency}>грн</span>
       </div>
-    </>
+    </div>
   );
 };
 
