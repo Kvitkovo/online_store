d97606074e73154bba10abd32b7d72ad5d8f6665
hrechanyktolia <tolikhrehanyk25@gmail.com>
2023-09-14 15:40:46 +0300

    inputSearch

--- frontend/src/pages/Promotions.js
+++ frontend/src/pages/Promotions.js
@@ -1,9 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
+import InputSearch from '../components/ui-kit/components/Input/InputSearch';
 
 function Promotions() {
+  //inputSearch
+  const [search, setSearch] = useState('');
+
+  const changeInput = (e) => {
+    setSearch(e.target.value);
+  };
+  const clearInput = () => {
+    setSearch('');
+  };
   return (
     <div>
       <h1>Promotions</h1>
+      <InputSearch
+        search={search}
+        changeInput={changeInput}
+        clearInput={clearInput}
+      />
     </div>
   );
 }
