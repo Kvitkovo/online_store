6924d360f24e8a90623b3007a65995cea40390f9
annaBalabak <annabalabak15@gmail.com>
2023-09-21 13:48:39 +0200

    corrected styles, added big screen styling

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,7 +7,7 @@ import Delivery from './pages/Delivery';
 import Handling from './pages/Handling';
 import './App.scss';
 import ROUTES from './constants/routers';
-import Header from './components/Layouts/Header/Header';
+import Header from './components/Layouts/Header';
 
 function App() {
   return (
