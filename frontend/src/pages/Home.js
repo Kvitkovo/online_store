da2847aa25650038925b41b3ec054cf2e9cd3c64
Svitlana-G <svitlana.golubieva@gmail.com>
2023-09-22 13:47:32 +0200

    Delete unnecessary elements from home page

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,50 +1,11 @@
 import React from 'react';
 import styles from '../scss/Home.module.scss';
-import Carousel from '../components/Carousel';
-import carouselData from '../data/carouselData.json';
-import DiscountPrice from '../components/ui-kit/components/DiscountPrice';
-import Card from '../components/common/Card';
+import HomePageComponent from '../components/Layouts/HomePageComponent';
 
 const Home = () => {
   return (
     <div className={styles.home}>
-      <Carousel data={carouselData.slides} />
-      <div className="padding">
-        <DiscountPrice oldPrice="2000" actualPrice="1500" isActive="ACTIVE" />
-        <DiscountPrice oldPrice="600" actualPrice="600" isActive="ACTIVE" />
-        <DiscountPrice
-          oldPrice="2000"
-          actualPrice="2000"
-          isActive="NO_ACTIVE"
-        />
-      </div>
-      <div className={styles.cardOutput}>
-        <Card
-          bouquet={false}
-          image="./images/bouquet.jpg"
-          title="Букет “101 троянда”"
-          discount={0}
-          oldPrice="1234"
-          price="1234"
-        />
-        <Card
-          image="./images/bouquet.jpg"
-          title="Троянда червона"
-          bouquet={true}
-          discount={10}
-          oldPrice="400"
-          price="200"
-        />
-        <Card
-          status="NO_ACTIVE"
-          bouquet={false}
-          image="./images/bouquet.jpg"
-          title="Букет “101 троянда”"
-          discount={10}
-          oldPrice="1234"
-          price="134"
-        />
-      </div>
+      <HomePageComponent />
     </div>
   );
 };
