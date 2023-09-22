e11f48edd9b5dd67f2984b0033e3a7d80baae16a
Svitlana-G <svitlana.golubieva@gmail.com>
2023-09-22 13:31:43 +0200

    Add HomePageComponent

---
+++ frontend/src/components/Layouts/HomePageComponent/HomePageComponent.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import CategoryOutput from './components/CategoryOutput';
+import Wrrapper from '../../Wrraper/Wrraper';
+
+const HomePageComponent = () => {
+  return (
+    <Wrrapper>
+      <CategoryOutput
+        title={'Акційна ціна'}
+        api={'v1/products/discounted?page=1&size=8'}
+        link={'#'}
+      />
+      <CategoryOutput
+        title={'Весільні букети'}
+        api={'v1/products/category?page=1&size=4&categoryId=2'}
+        link={'#'}
+      />
+      <CategoryOutput
+        title={'Квіти у кошику'}
+        api={'v1/products/category?page=1&size=4&categoryId=28'}
+        link={'#'}
+      />
+      <CategoryOutput
+        title={'Кімнатні квіти'}
+        api={'v1/products/category?page=1&size=4&categoryId=29'}
+        link={'#'}
+      />
+    </Wrrapper>
+  );
+};
+export default HomePageComponent;
