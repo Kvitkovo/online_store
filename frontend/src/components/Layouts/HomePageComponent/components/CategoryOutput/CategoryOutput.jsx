392b2fa8f63518bb8508bb1b4337ba665d003e69
Svitlana-G <svitlana.golubieva@gmail.com>
2023-09-25 15:35:11 +0200

    Add loading status

--- frontend/src/components/Layouts/HomePageComponent/components/CategoryOutput/CategoryOutput.jsx
+++ frontend/src/components/Layouts/HomePageComponent/components/CategoryOutput/CategoryOutput.jsx
@@ -8,42 +8,50 @@ import IconButton from '../../../../ui-kit/components/IconButton';
 
 const CategoryOutput = ({ api, link }) => {
   const [data, setData] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
   useEffect(() => {
     axiosInstance
       .get(api)
       .then((response) => {
         setData(response.data.content);
+        setIsLoading(true);
       })
       .catch((error) => {
         console.error(error);
       });
   }, [api]);
   return (
-    <div className={styles.categoryOutput}>
-      <div className={styles.cardOutput}>
-        {data &&
-          data.map((card) => (
-            <Card
-              image={
-                card.images[0]
-                  ? card.images[0].urlSmall
-                  : './images/no_image.jpg'
-              }
-              title={card.title}
-              discount={card.discount}
-              oldPrice={card.priceWithDiscount}
-              price={card.price}
-              key={card.id}
-            />
-          ))}
-      </div>
-      <div className={styles.flexLink}>
-        <Link to={link} className={styles.link}>
-          Показати усі
-        </Link>
-        <IconButton icon={<ICONS.hideList />} />
-      </div>
-    </div>
+    <>
+      {!isLoading ? (
+        <p>Loading...</p>
+      ) : (
+        <div className={styles.categoryOutput}>
+          <div className={styles.cardOutput}>
+            {data &&
+              data.map((card) => (
+                <Card
+                  image={
+                    card.images[0]
+                      ? card.images[0].urlSmall
+                      : './images/no_image.jpg'
+                  }
+                  title={card.title}
+                  discount={card.discount}
+                  oldPrice={card.priceWithDiscount}
+                  price={card.price}
+                  key={card.id}
+                />
+              ))}
+          </div>
+          <div className={styles.flexLink}>
+            <Link to={link} className={styles.link}>
+              Показати усі
+            </Link>
+            <IconButton icon={<ICONS.hideList />} />
+          </div>
+        </div>
+      )}
+    </>
   );
 };
 
