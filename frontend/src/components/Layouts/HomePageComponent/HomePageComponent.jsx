2dd5a360aaf4a75cce3d0da3e459b97f2c1a4f66
Svitlana-G <svitlana.golubieva@gmail.com>
2023-09-25 15:40:19 +0200

    Put links in const

--- frontend/src/components/Layouts/HomePageComponent/HomePageComponent.jsx
+++ frontend/src/components/Layouts/HomePageComponent/HomePageComponent.jsx
@@ -6,28 +6,20 @@ import carouselData from '../../../data/carouselData.json';
 import styles from './HomePageComponent.module.scss';
 
 const HomePageComponent = () => {
+  const categoryAPI = 'v1/products/category?page=1&size=4&categoryId=';
   return (
     <Wrrapper>
       <Carousel data={carouselData.slides} />
       <h2 className={styles.title}>
         <span>Акційна</span> ціна
       </h2>
-      <CategoryOutput api={'v1/products/discounted?page=1&size=8'} link={'#'} />
+      <CategoryOutput api="v1/products/discounted?page=1&size=8" link={'#'} />
       <h2 className={styles.title}>Весільні букети</h2>
-      <CategoryOutput
-        api={'v1/products/category?page=1&size=4&categoryId=2'}
-        link={'#'}
-      />
+      <CategoryOutput api={categoryAPI + '2'} link={'#'} />
       <h2 className={styles.title}>Квіти у кошику</h2>
-      <CategoryOutput
-        api={'v1/products/category?page=1&size=4&categoryId=28'}
-        link={'#'}
-      />
+      <CategoryOutput api={categoryAPI + '28'} link={'#'} />
       <h2 className={styles.title}>Кімнатні квіти</h2>
-      <CategoryOutput
-        api={'v1/products/category?page=1&size=4&categoryId=29'}
-        link={'#'}
-      />
+      <CategoryOutput api={categoryAPI + '29'} link={'#'} />
     </Wrrapper>
   );
 };
