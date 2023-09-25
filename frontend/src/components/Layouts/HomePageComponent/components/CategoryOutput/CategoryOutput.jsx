9c87c7a639468ebd7365a734ea3adecd871efe05
Svitlana-G <svitlana.golubieva@gmail.com>
2023-09-25 14:48:27 +0200

    Add initial state

--- frontend/src/components/Layouts/HomePageComponent/components/CategoryOutput/CategoryOutput.jsx
+++ frontend/src/components/Layouts/HomePageComponent/components/CategoryOutput/CategoryOutput.jsx
@@ -7,7 +7,7 @@ import { Link } from 'react-router-dom';
 import IconButton from '../../../../ui-kit/components/IconButton';
 
 const CategoryOutput = ({ api, link }) => {
-  const [data, setData] = useState();
+  const [data, setData] = useState([]);
   useEffect(() => {
     axiosInstance
       .get(api)
