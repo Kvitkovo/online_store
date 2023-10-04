8ad4a453b419a525eea629b52ed1917a2ee2f92b
Svitlana-G <svitlana.golubieva@gmail.com>
2023-10-04 14:11:43 +0200

    Add block with  contact details

--- frontend/src/components/account/ContactDetails/ContactDetails.jsx
+++ frontend/src/components/account/ContactDetails/ContactDetails.jsx
@@ -20,7 +20,25 @@ const ContactDetails = () => {
           <div className={styles.menuItem}>Вихід</div>
         </div>
         <div className={styles.contactDetails}>
-          <h1> Контактні дані</h1>
+          <h2> Контактні дані</h2>
+          <div className={styles.flexFieldsAndData}>
+            <div className={styles.fields}>
+              <p>Ім&apos;я</p>
+              <p>Прізвище</p>
+              <p>По батькові</p>
+              <p>Номер телефону</p>
+              <p>Ел. пошта</p>
+              <p>Дата народження</p>
+            </div>
+            <div className={styles.data}>
+              <p>Олена</p>
+              <p>Шевченко</p>
+              <p>Олегівна</p>
+              <p>+380670000000</p>
+              <p>example@gmail.com</p>
+              <p>26.03.1992</p>
+            </div>
+          </div>
         </div>
       </div>
     </Wrapper>
