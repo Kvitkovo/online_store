750f91e9bb90f244d4a3840700ba0df7cce38f76
annaBalabak <annabalabak15@gmail.com>
2023-10-06 19:48:08 +0200

    added useLocation and useEffect to listen to url changes

--- frontend/src/components/Layouts/Header/components/BurgerMenu/BurgerMenu.jsx
+++ frontend/src/components/Layouts/Header/components/BurgerMenu/BurgerMenu.jsx
@@ -1,13 +1,19 @@
-import React, { useCallback, useState } from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import OpenMenu from './OpenMenu';
 import ClosedMenu from './ClosedMenu';
+import { useLocation } from 'react-router-dom';
 
 const BurgerMenu = () => {
   const [isOpen, setIsOpen] = useState(false);
+  const location = useLocation();
 
   const toggleMenu = useCallback(() => {
     setIsOpen((prevIsOpen) => !prevIsOpen);
   }, []);
+
+  useEffect(() => {
+    setIsOpen(false);
+  }, [location.pathname]);
   return (
     <>
       {isOpen ? (
