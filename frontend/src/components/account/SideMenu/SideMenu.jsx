0a4c7f9401519512d63e050f4f6e6bb5d22d5e55
Svitlana-G <svitlana.golubieva@gmail.com>
2023-10-06 13:41:55 +0200

    Update side menu for account

--- frontend/src/components/account/SideMenu/SideMenu.jsx
+++ frontend/src/components/account/SideMenu/SideMenu.jsx
@@ -1,25 +1,20 @@
 import React from 'react';
 import styles from './SideMenu.module.scss';
-import { ICONS } from '../../ui-kit/icons';
 import { NavLink } from 'react-router-dom';
 
 const SideMenu = () => {
   return (
     <div className={styles.menuAccount}>
-      <div className={styles.account}>
-        <div className={styles.accountIcon}>
-          {' '}
-          <ICONS.account />
-        </div>
-        <div className={styles.name}>Шевченко Олена Олегівна</div>
-      </div>
       <NavLink to="/account" className={styles.menuItem}>
         Контактні дані
       </NavLink>
       <NavLink to="/account/orders" className={styles.menuItem}>
         Мої замовлення
       </NavLink>
-      <div className={styles.menuItem}>Вихід</div>
+      <hr />
+      <NavLink to="/account/orders" className={styles.menuItem}>
+        Вихід
+      </NavLink>
     </div>
   );
 };
