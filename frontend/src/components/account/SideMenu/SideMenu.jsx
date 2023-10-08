9b27b4b61e7f3945bb47dc3e12c45475f3929e2e
Svitlana-G <svitlana.golubieva@gmail.com>
2023-10-08 16:32:59 +0200

    Update side menu for account

--- frontend/src/components/account/SideMenu/SideMenu.jsx
+++ frontend/src/components/account/SideMenu/SideMenu.jsx
@@ -1,17 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './SideMenu.module.scss';
 import { NavLink } from 'react-router-dom';
 
 const SideMenu = () => {
+  const [active, setActive] = useState(true);
+
   return (
     <div className={styles.menuAccount}>
-      <NavLink to="/account" className={styles.menuItem}>
+      <NavLink
+        to="/account"
+        className={active ? `${styles.active}` : `${styles.inactive}`}
+        onClick={() => setActive(true)}
+      >
         Контактні дані
       </NavLink>
-      <NavLink to="/account/orders" className={styles.menuItem}>
+      <NavLink
+        to="/account/orders"
+        className={!active ? `${styles.active}` : `${styles.inactive}`}
+        onClick={() => setActive(false)}
+      >
         Мої замовлення
       </NavLink>
-      <hr />
       <NavLink to="/account/orders" className={styles.menuItem}>
         Вихід
       </NavLink>
