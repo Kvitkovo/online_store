a7b1e9397eb60d8a8eb88ce5536de37caaa565e3
Svitlana-G <svitlana.golubieva@gmail.com>
2023-10-11 13:19:35 +0200

    Update component ContactDetails

--- frontend/src/components/account/ContactDetails/ContactDetails.jsx
+++ frontend/src/components/account/ContactDetails/ContactDetails.jsx
@@ -3,7 +3,7 @@ import styles from './ContactDetails.module.scss';
 import { ICONS } from '../../ui-kit/icons';
 import Button from '../../ui-kit/components/Button/Button';
 import { useNavigate } from 'react-router-dom';
-import SideMenu from '../SideMenu/SideMenu';
+import Account from '../Account';
 
 const ContactDetails = () => {
   const navigate = useNavigate();
@@ -17,47 +17,43 @@ const ContactDetails = () => {
   };
 
   return (
-    <>
-      <h2 className={styles.accountTitle}>Вітаємо, Олена</h2>
-      <div className={styles.gridTwoBlocks}>
-        <SideMenu />
-        <div className={styles.contactDetails}>
-          <h2 className={styles.accountTitle}> Контактні дані</h2>
-          <div className={styles.flexFieldsAndData}>
-            <div className={styles.fields}>
-              <p>Ім&apos;я</p>
-              <p>Прізвище</p>
-              <p>По батькові</p>
-              <p>Номер телефону</p>
-              <p>Ел. пошта</p>
-              <p>Дата народження</p>
-            </div>
-            <div className={styles.data}>
-              <p>Олена</p>
-              <p>Шевченко</p>
-              <p>Олегівна</p>
-              <p>+380670000000</p>
-              <p>example@gmail.com</p>
-              <p>26.03.1992</p>
-            </div>
+    <Account title="Вітаємо, Олена">
+      <div className={styles.block}>
+        <h2 className={styles.accountTitle}> Контактні дані</h2>
+        <div className={styles.flexFieldsAndData}>
+          <div className={styles.fields}>
+            <p>Ім&apos;я</p>
+            <p>Прізвище</p>
+            <p>По батькові</p>
+            <p>Номер телефону</p>
+            <p>Ел. пошта</p>
+            <p>Дата народження</p>
           </div>
-          <div className={styles.flexButtons}>
-            <Button
-              variant="no-border"
-              label="Редагувати"
-              icon={<ICONS.PencilIcon />}
-              onClick={navigateToEdit}
-            />
-            <Button
-              variant="no-border"
-              label="Змінити пароль"
-              icon={<ICONS.changePassword />}
-              onClick={navigateToChangePassword}
-            />
+          <div className={styles.data}>
+            <p>Олена</p>
+            <p>Шевченко</p>
+            <p>Олегівна</p>
+            <p>+380670000000</p>
+            <p>example@gmail.com</p>
+            <p>26.03.1992</p>
           </div>
         </div>
+        <div className={styles.flexButtons}>
+          <Button
+            variant="no-border"
+            label="Редагувати"
+            icon={<ICONS.PencilIcon />}
+            onClick={navigateToEdit}
+          />
+          <Button
+            variant="no-border"
+            label="Змінити пароль"
+            icon={<ICONS.changePassword />}
+            onClick={navigateToChangePassword}
+          />
+        </div>
       </div>
-    </>
+    </Account>
   );
 };
 export default ContactDetails;
