3be0b835e7062cded071202e3a90d2a3cbcf763d
Svitlana-G <svitlana.golubieva@gmail.com>
2023-10-11 13:57:53 +0200

    Add code for case no orders

--- frontend/src/components/account/Orders/Orders.jsx
+++ frontend/src/components/account/Orders/Orders.jsx
@@ -1,7 +1,27 @@
 import React from 'react';
+import styles from './Orders.module.scss';
+import Account from '../Account';
 
 const Orders = () => {
-  return <div>Orders</div>;
+  return (
+    <Account title="Вітаємо, Олена">
+      <div className={styles.orders}>
+        <h2 className={styles.title}> Мої замовлення</h2>
+        <div className={styles.gridTable}>
+          <div>Номер</div>
+          <div>Дата</div>
+          <div>Отримувач</div>
+          <div>Сума</div>
+          <div>Статус</div>
+          <div></div>
+        </div>
+        <div className={styles.noOrders}>
+          <p>У вас поки що немає замовлень.</p>
+          <img src="/images/no_orders.jpg" alt="" />
+        </div>
+      </div>
+    </Account>
+  );
 };
 
 export default Orders;
