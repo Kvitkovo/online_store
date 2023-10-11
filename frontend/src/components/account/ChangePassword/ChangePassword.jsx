e6c2f6b5d8936b70e6ad30d22a49b8fa82aff976
Svitlana-G <svitlana.golubieva@gmail.com>
2023-10-11 13:23:18 +0200

    Update component ChangePassword

--- frontend/src/components/account/ChangePassword/ChangePassword.jsx
+++ frontend/src/components/account/ChangePassword/ChangePassword.jsx
@@ -1,6 +1,11 @@
 import React from 'react';
-
+import Account from '../Account';
+import styles from './ChangePassword.module.scss';
 const ChangePassword = () => {
-  return <div>Змінити пароль</div>;
+  return (
+    <Account title="Редагування особистої інформації">
+      <h2 className={styles.title}> Зміна паролю</h2>
+    </Account>
+  );
 };
 export default ChangePassword;
