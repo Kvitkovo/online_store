4cf52330b9eb33c2b54a13eb20dbae46e74c38ee
annaBalabak <annabalabak15@gmail.com>
2023-10-21 16:09:57 +0200

    minor changes

--- frontend/src/pages/CardPage/components/ItemFeatures/ItemFeatures.jsx
+++ frontend/src/pages/CardPage/components/ItemFeatures/ItemFeatures.jsx
@@ -4,17 +4,19 @@ import styles from './ItemFeatures.module.scss';
 const ItemFeatures = ({ type, color, size }) => {
   return (
     <div className={styles.features}>
-      <p>
-        Вид квітів: <span className={styles.type}> {type} </span>
-      </p>
-
-      <p>
-        Колір: <span className={styles.type}> {color} </span>{' '}
-      </p>
-
+      {type && (
+        <p>
+          Вид квітів: <span className={styles.type}> {type} </span>
+        </p>
+      )}
+      {color && (
+        <p>
+          Колір: <span className={styles.type}> {color} </span>
+        </p>
+      )}
       {size && (
         <p>
-          Висота букета: <span className={styles.type}> {size} </span>{' '}
+          Висота букета: <span className={styles.type}> {size} </span>
         </p>
       )}
     </div>
