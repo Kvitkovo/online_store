aabd8f4c8f05b62754afc29bd332148f42e38c51
Svitlana-G <svitlana.golubieva@gmail.com>
2023-10-21 22:09:53 +0200

    Add component RecipientDetails

--- frontend/src/components/account/Orders/components/OrderItem/OrderItem.jsx
+++ frontend/src/components/account/Orders/components/OrderItem/OrderItem.jsx
@@ -1,57 +1,15 @@
 import React from 'react';
 import styles from './OrderItem.module.scss';
 
-const OrderItem = ({
-  number,
-  code,
-  item,
-  itemQuantity,
-  price,
-  city,
-  street,
-  house,
-  apartment,
-  recipient,
-  phone,
-  quantity,
-}) => {
+const OrderItem = ({ number, code, item, itemQuantity, price }) => {
   return (
-    <>
-      <div className={styles.item}></div>
-      <div className={styles.gridOrder}>
-        <div>1 {number}</div>
-        <div>код {code}</div>
-        <div>{item}</div>
-        <div>{itemQuantity} шт</div>
-        <div>{price} грн</div>
-      </div>
-      <div className={styles.item}></div>
-      <div className={styles.flexData}>
-        <div>
-          <div className={styles.address}>
-            <div>Доставка:</div>
-            <div>
-              <span>місто</span> {city}
-            </div>
-            <div>
-              <span>вул.</span> {street} <span>буд.</span> {house}{' '}
-              <span>кв.</span> {apartment}
-            </div>
-          </div>
-          <div className={styles.recipient}>
-            <div>Отримувач:</div>
-            <div>{recipient}</div>
-            <div>
-              <span>Тел.</span> {phone}{' '}
-            </div>
-          </div>
-        </div>
-        <div className={styles.quantity}>
-          <div>Кількість товарів: </div>
-          <div>{quantity} шт</div>
-        </div>
-      </div>
-    </>
+    <div className={styles.gridOrder}>
+      <div>{number}</div>
+      <div>код {code}</div>
+      <div>{item}</div>
+      <div>{itemQuantity} шт</div>
+      <div>{price} грн</div>
+    </div>
   );
 };
 export default OrderItem;
---
+++ frontend/src/components/account/Orders/components/RecipientDetails/RecipientDetails.jsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import styles from './RecipientDetails.module.scss';
+
+const RecipientDetails = ({
+  city,
+  street,
+  house,
+  apartment,
+  recipient,
+  phone,
+  quantity,
+}) => {
+  return (
+    <>
+      <div className={styles.flexData}>
+        <div>
+          <div className={styles.address}>
+            <div>Доставка:</div>
+            <div>
+              <span>місто</span> {city}
+            </div>
+            <div>
+              <span>вул.</span> {street} <span>буд.</span> {house}{' '}
+              <span>кв.</span> {apartment}
+            </div>
+          </div>
+          <div className={styles.recipient}>
+            <div>Отримувач:</div>
+            <div>{recipient}</div>
+            <div>
+              <span>Тел.</span> {phone}{' '}
+            </div>
+          </div>
+        </div>
+        <div className={styles.quantity}>
+          <div>Кількість товарів: </div>
+          <div>{quantity} шт</div>
+        </div>
+      </div>
+    </>
+  );
+};
+export default RecipientDetails;
