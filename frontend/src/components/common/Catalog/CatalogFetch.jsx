bba84fa5b168bdf5189ef563eaedf876e3b14bb7
Svitlana-G <svitlana.golubieva@gmail.com>
2023-10-24 08:51:01 +0200

    Add object for category sale

--- frontend/src/components/common/Catalog/CatalogFetch.jsx
+++ frontend/src/components/common/Catalog/CatalogFetch.jsx
@@ -11,7 +11,38 @@ const CatalogFetch = ({ setIsOpen }) => {
   useEffect(() => {
     const getCatalog = async () => {
       const response = await GetCategories(sortValue);
-      setCategoryData(response);
+      setCategoryData(
+        response.push({
+          id: 31,
+          name: 'Акційна ціна',
+          alias: 'Sale',
+          parent: null,
+          metaDescription: '',
+          metaKeywords: '',
+          description: '',
+          status: 'ACTIVE',
+          icon: 'SALE',
+          hasSubCategory: false,
+          sortValue: 31,
+        }),
+      );
+      // console.log(response);
+      // console.log(
+      //   response.push({
+      //     id: 0,
+      //     name: 'Акційна ціна',
+      //     alias: 'Sale',
+      //     parent: null,
+      //     metaDescription: '',
+      //     metaKeywords: '',
+      //     description: '',
+      //     status: 'ACTIVE',
+      //     icon: 'SALE',
+      //     hasSubCategory: false,
+      //     sortValue: 31,
+      //   }),
+      // );
+      // console.log(response);
     };
     getCatalog();
   }, [sortValue]);
