69a8f7c34da365ee1b73588045fb9027353224d1
Svitlana-G <svitlana.golubieva@gmail.com>
2023-11-03 17:26:41 +0100

    Add the counts of products in the cart

--- frontend/src/components/Layouts/Header/components/BurgerMenu/ClosedMenu.jsx
+++ frontend/src/components/Layouts/Header/components/BurgerMenu/ClosedMenu.jsx
@@ -1,9 +1,17 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import styles from './BurgerMenu.module.scss';
 import { ICONS } from '../../../../ui-kit/icons';
 import IconButton from '../../../../ui-kit/components/IconButton';
+import { useDispatch, useSelector } from 'react-redux';
+import { calculateTotal } from '../../../../../redux/slices/cartSlice';
 
 const ClosedMenu = ({ toggleMenu, toggleCart, toggleMyBouquet }) => {
+  const cart = useSelector((state) => state.cartSliceReducer);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(calculateTotal());
+  }, [cart, dispatch]);
+
   return (
     <div className={styles.burgerMenu}>
       <div className={styles.burgerHeader}>
@@ -25,6 +33,7 @@ const ClosedMenu = ({ toggleMenu, toggleCart, toggleMyBouquet }) => {
           </div>
           <div className={styles.cart}>
             <IconButton icon={<ICONS.CartIcon />} onClick={toggleCart} />
+            <div className={styles.cartQuantity}>{cart.cartTotalQuantity}</div>
           </div>
         </div>
       </div>
