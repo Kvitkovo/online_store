b929178f18c762e919da8db4ce72984ee7ca26ee
Svitlana-G <svitlana.golubieva@gmail.com>
2023-11-22 13:16:44 +0100

    Delete unnesessary condition

--- frontend/src/utils/isItemInCart.js
+++ frontend/src/utils/isItemInCart.js
@@ -1,6 +1,3 @@
 export const isItemInCart = (cartItems, itemId) => {
-  if (cartItems && cartItems.length > 0) {
-    return cartItems.some((el) => el.id === itemId);
-  }
-  return false;
+  return cartItems.some((el) => el.id === itemId);
 };
