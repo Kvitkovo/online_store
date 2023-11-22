16fded45a959035ed6d6d09f81ba2425b20cb894
Alya Rydasova <69515902+halynarydasova@users.noreply.github.com>
2023-11-22 14:38:11 +0200

    added logic for path component

--- frontend/src/pages/CardPage/components/Path/Path.jsx
+++ frontend/src/pages/CardPage/components/Path/Path.jsx
@@ -1,14 +1,63 @@
-import React from 'react';
+/* eslint-disable max-len */
+import React, { useCallback, useEffect, useState } from 'react';
 import styles from './Path.module.scss';
+import ROUTES from '../../../../constants/routers';
 
-const Path = () => {
+import { Link, useParams } from 'react-router-dom';
+import { GetCategory } from '../../../../services/catalog/categoryAccess.service';
+import { ICONS } from '../../../../components/ui-kit/icons';
+
+const Path = React.memo(({ currentPageData = {} }) => {
+  const params = useParams();
+  const currentPageType = params.categoryId ? 'category' : 'product';
+  const [currentPage, setCurrentPage] = useState(null);
+  const findCurrentPage = useCallback(async () => {
+    if (currentPageType === 'category') {
+      const result = await GetCategory(params.categoryId);
+      setCurrentPage(result);
+    } else {
+      setCurrentPage(currentPageData);
+    }
+  }, [currentPageData, currentPageType, params.categoryId]);
+
+  useEffect(() => {
+    findCurrentPage();
+  });
+
   return (
     <div className={styles.pathContainer}>
-      <p className={styles.path}>
-        Головна <span> &gt; </span> Букети з квітів <span> &gt;</span> Букет
-        “101” троянда
-      </p>
+      <nav className={styles.path}>
+        <Link to={ROUTES.home} className={styles.navigationLink}>
+          Головна
+        </Link>
+        {currentPage && (
+          <>
+            <ICONS.arrowRight />
+            {currentPageType === 'category' ? (
+              <span className={styles.navigation}>{currentPage.name}</span>
+            ) : (
+              <Link
+                to={`/categories/${
+                  currentPageType === 'category'
+                    ? params.categoryId
+                    : currentPage.categoryId
+                }`}
+                className={styles.navigationLink}
+              >
+                {' '}
+                {currentPage.categoryName}
+              </Link>
+            )}
+            {currentPageType === 'product' && (
+              <>
+                <ICONS.arrowRight />
+                <span className={styles.navigation}>{currentPage.title}</span>
+              </>
+            )}
+          </>
+        )}
+      </nav>
     </div>
   );
-};
+});
 export default Path;
