85f56d6f3f936a8505367dd5c61593cb8e88137d
Svitlana-G <svitlana.golubieva@gmail.com>
2023-11-22 12:05:26 +0100

    Add button for a product that has been added to the cart

--- frontend/src/pages/CardPage/components/PriceAndButtons/PriceAndButtons.jsx
+++ frontend/src/pages/CardPage/components/PriceAndButtons/PriceAndButtons.jsx
@@ -12,6 +12,7 @@ const PriceAndButtons = ({
   stockInfo,
   addToConstructor,
   addToCart,
+  inCart,
 }) => {
   const isInStock = stockInfo === 'AVAILABLE';
 
@@ -24,13 +25,24 @@ const PriceAndButtons = ({
             <DiscountPrice oldPrice={oldPrice} actualPrice={actualPrice} />
           </div>
           <div className={styles.buttons}>
-            <Button
-              variant="primary"
-              label="Додати у кошик"
-              padding="padding-sm"
-              icon={<ICONS.toCart />}
-              onClick={addToCart}
-            />
+            {!inCart && (
+              <Button
+                variant="primary"
+                label="Додати у кошик"
+                padding="padding-sm"
+                icon={<ICONS.toCart />}
+                onClick={addToCart}
+              />
+            )}
+            {inCart && (
+              <Button
+                variant="primary"
+                label="У кошику"
+                padding="padding-sm"
+                icon={<ICONS.cartChecked />}
+                onClick={addToCart}
+              />
+            )}
             {addToBouquete ? (
               <>
                 <div className={styles.bouquetDesktop}>
