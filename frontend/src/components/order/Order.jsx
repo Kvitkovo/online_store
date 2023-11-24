27422dfeabb6f3b7e69751de5cd1e84012598d4f
Svitlana-G <svitlana.golubieva@gmail.com>
2023-11-24 19:46:02 +0100

    Add mixin for title

--- frontend/src/components/order/Order.jsx
+++ frontend/src/components/order/Order.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import styles from './Order.module.scss';
 
 const Order = () => {
   return (
     <div>
-      <h2>Оформлення замовлення</h2>
+      <h2 className={styles.title}>Оформлення замовлення</h2>
     </div>
   );
 };
