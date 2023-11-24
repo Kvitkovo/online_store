1253ae02416b5f3be7c75a20d1aef927d7af3d0a
Svitlana-G <svitlana.golubieva@gmail.com>
2023-11-24 20:04:55 +0100

    Add two blocks and titles for them

--- frontend/src/components/order/Order.jsx
+++ frontend/src/components/order/Order.jsx
@@ -3,8 +3,18 @@ import styles from './Order.module.scss';
 
 const Order = () => {
   return (
-    <div>
+    <div className={styles.order}>
       <h2 className={styles.title}>Оформлення замовлення</h2>
+      <div className={styles.mainGrid}>
+        <div className={styles.contactDetails}>
+          <h3 className={styles.subtitle}>
+            <span>1.</span>Контактні дані
+          </h3>
+        </div>
+        <div className={styles.cart}>
+          <h3 className={styles.subtitle}>Інформація про замовлення</h3>
+        </div>
+      </div>
     </div>
   );
 };
