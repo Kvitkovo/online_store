51fbea6096cbf1999cc014d0fdb40f80e603de00
gaponov <wmconstructor@gmail.com>
2023-12-15 20:55:08 +0200

    refactor

--- backend/src/main/java/ua/kvitkovo/notifications/NotificationType.java
+++ backend/src/main/java/ua/kvitkovo/notifications/NotificationType.java
@@ -1,8 +1,5 @@
 package ua.kvitkovo.notifications;
 
-/**
- * @author Andriy Gaponov
- */
 public enum NotificationType {
     MAIL_CONFIRMATION, MAIL_CONFIRMATION_SUCCESSFULLY, RESET_PASSWORD, CHANGE_PASSWORD, CREATE_NEW_USER, ANSWER_FEEDBACK_MESSAGE
 }
