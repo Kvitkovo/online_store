c58bf428bde6ef3225c994d74893d88246e66bb1
Halyna Rydasova <halynarydasova@Halynas-MacBook-Pro.local>
2024-01-09 11:02:02 +0200

    fixed positioning for select component

--- frontend/src/components/ui-kit/components/Select/Select.jsx
+++ frontend/src/components/ui-kit/components/Select/Select.jsx
@@ -10,30 +10,35 @@ const Select = memo(({ value, setValue, options }) => {
     setValue(nextValue);
     setOpen(false);
   };
+  const handleMouseLeave = () => {
+    setOpen(false);
+  };
   return (
-    <div className={styles.root}>
-      <div className={styles.select} onClick={() => setOpen(!open)}>
-        <span className={styles.label}>
-          {options.find((option) => option.value === value)?.label}
-        </span>
-        {open ? <ICONS.arrowUp /> : <ICONS.showList />}
-      </div>
-      {open && (
-        <div className={styles.option}>
-          {options.map(
-            (option) =>
-              option.value !== value && (
-                <span
-                  key={option.value}
-                  className={styles.label}
-                  onClick={() => handleChange(option.value)}
-                >
-                  {option.label}
-                </span>
-              ),
-          )}
+    <div className={styles.container}>
+      <div className={styles.root} onMouseLeave={handleMouseLeave}>
+        <div className={styles.select} onClick={() => setOpen(!open)}>
+          <span className={styles.label}>
+            {options.find((option) => option.value === value)?.label}
+          </span>
+          {open ? <ICONS.arrowUp /> : <ICONS.showList />}
         </div>
-      )}
+        {open && (
+          <div className={styles.option}>
+            {options.map(
+              (option) =>
+                option.value !== value && (
+                  <span
+                    key={option.value}
+                    className={styles.label}
+                    onClick={() => handleChange(option.value)}
+                  >
+                    {option.label}
+                  </span>
+                ),
+            )}
+          </div>
+        )}
+      </div>
     </div>
   );
 });
