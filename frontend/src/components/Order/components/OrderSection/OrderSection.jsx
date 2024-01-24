4101071de253ac69839f00e4bd52023c804b0e0b
Svitlana-G <svitlana.golubieva@gmail.com>
2024-01-24 18:06:21 +0100

    Add styles for order section

--- frontend/src/components/Order/components/OrderSection/OrderSection.jsx
+++ frontend/src/components/Order/components/OrderSection/OrderSection.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import styles from './OrderSection.scss';
+import styles from './OrderSection.module.scss';
 import Button from '../../../ui-kit/components/Button';
 import { ICONS } from '../../../ui-kit/icons';
 
