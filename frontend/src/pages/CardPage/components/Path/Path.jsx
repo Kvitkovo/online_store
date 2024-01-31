22083d4e697adda8a470962c6688f2cabb48c46d
Alya Rydasova <halyna.rydasova@gmail.com>
2024-01-31 09:33:28 +0200

    fixed path

--- frontend/src/pages/CardPage/components/Path/Path.jsx
+++ frontend/src/pages/CardPage/components/Path/Path.jsx
@@ -1,11 +1,35 @@
 /* eslint-disable max-len */
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import styles from './Path.module.scss';
 import ROUTES from '../../../../constants/routers';
 import { Link } from 'react-router-dom';
 import { ICONS } from '../../../../components/ui-kit/icons';
+import { GetCategory } from '../../../../services/catalog/categoryAccess.service';
 
 const Path = React.memo(({ currentPageData, currentPageType }) => {
+  const [parents, setParents] = useState([]);
+
+  useEffect(() => {
+    const fetchCategoryAndSubcategory = async () => {
+      const categoryId = currentPageData.categoryId;
+      const category = await GetCategory(categoryId);
+      const parents = [];
+      let parent = category;
+      while (parent !== null) {
+        const { id, name } = parent;
+        parents.push({ id, name });
+        parent = parent ? parent.parent : null;
+      }
+      setParents(parents.reverse());
+    };
+    if (currentPageType === 'product') {
+      fetchCategoryAndSubcategory();
+    }
+    return () => {
+      setParents([]);
+    };
+  }, [currentPageData.categoryId, currentPageType]);
+
   return (
     <div className={styles.pathContainer}>
       <nav className={styles.path}>
@@ -14,20 +38,35 @@ const Path = React.memo(({ currentPageData, currentPageType }) => {
         </Link>
         {currentPageData && (
           <>
-            <div className={styles.arrow}>
-              <ICONS.pathArrow />
-            </div>
-            {currentPageType === 'category' || currentPageType === 'section' ? (
-              <span className={styles.navigation}>{currentPageData.name}</span>
-            ) : (
-              <Link
-                to={`/categories/${currentPageData.categoryId}`}
-                className={styles.navigationLink}
-              >
-                {' '}
-                {currentPageData.categoryName}
-              </Link>
-            )}
+            {currentPageType === 'category' ||
+              (currentPageType === 'section' && (
+                <>
+                  <div className={styles.arrow}>
+                    <ICONS.pathArrow />
+                  </div>
+                  <span className={styles.navigation}>
+                    {currentPageData.name}
+                  </span>
+                </>
+              ))}
+            {parents.length > 0 &&
+              parents.map((parent) => {
+                return (
+                  <>
+                    <div className={styles.arrow}>
+                      <ICONS.pathArrow />
+                    </div>
+                    <Link
+                      key={parent.id}
+                      to={`/categories/1`}
+                      className={styles.navigationLink}
+                    >
+                      {parent.name}
+                    </Link>
+                  </>
+                );
+              })}
+
             {currentPageType === 'product' && (
               <>
                 <div className={styles.arrow}>
