f314f07b40ee4dc8fbbd6b353a10985185697945
Svitlana-G <svitlana.golubieva@gmail.com>
2024-02-08 11:55:01 +0100

    Add radio buttons add postcard and no postcard

--- frontend/src/components/Order/components/PostcardAndComment/PostcardAndComment.jsx
+++ frontend/src/components/Order/components/PostcardAndComment/PostcardAndComment.jsx
@@ -1,7 +1,34 @@
 import React from 'react';
-import styles from './PostcardAndComment';
+import { useForm } from 'react-hook-form';
+import styles from './PostcardAndComment.module.scss';
 
-const PostcardAndComment = () => {
-  return <div className={styles.title}>Postcard and Comment</div>;
+const PostcardAndComment = ({ postcardData }) => {
+  const { register } = useForm({
+    defaultValues: {
+      postcard: postcardData?.postcard ? postcardData.postCard : 'Add postcard',
+    },
+  });
+  return (
+    <form>
+      <label htmlFor="postcard" className={styles.radioBtn}>
+        <input
+          type="radio"
+          id="postcard"
+          value="Add postcard"
+          {...register('postcard')}
+        />
+        Додати листівку з побажанням
+      </label>
+      <label htmlFor="noPostcard" className={styles.radioBtn}>
+        <input
+          type="radio"
+          id="noPostcard"
+          value="No postcard"
+          {...register('postcard')}
+        />
+        Без листівки
+      </label>
+    </form>
+  );
 };
 export default PostcardAndComment;
