7a4b1d5769492c057d28863cc1c055ec3ac98e0e
Svitlana-G <svitlana.golubieva@gmail.com>
2024-02-08 12:02:06 +0100

    Add textarea for postcard

--- frontend/src/components/Order/components/PostcardAndComment/PostcardAndComment.jsx
+++ frontend/src/components/Order/components/PostcardAndComment/PostcardAndComment.jsx
@@ -1,8 +1,9 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useForm } from 'react-hook-form';
 import styles from './PostcardAndComment.module.scss';
 
 const PostcardAndComment = ({ postcardData }) => {
+  const [showPostcardTextarea, setShowPostcardTextarea] = useState(true);
   const { register } = useForm({
     defaultValues: {
       postcard: postcardData?.postcard ? postcardData.postCard : 'Add postcard',
@@ -16,15 +17,27 @@ const PostcardAndComment = ({ postcardData }) => {
           id="postcard"
           value="Add postcard"
           {...register('postcard')}
+          onChange={() => {
+            setShowPostcardTextarea(true);
+          }}
         />
         Додати листівку з побажанням
       </label>
+      {showPostcardTextarea && (
+        <textarea
+          placeholder="Напишіть ваше побажання"
+          {...register('postcardText')}
+        ></textarea>
+      )}
       <label htmlFor="noPostcard" className={styles.radioBtn}>
         <input
           type="radio"
           id="noPostcard"
           value="No postcard"
           {...register('postcard')}
+          onChange={() => {
+            setShowPostcardTextarea(false);
+          }}
         />
         Без листівки
       </label>
