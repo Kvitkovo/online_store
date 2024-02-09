52a13504cf3b08e3854d0b1e4991e4369e0bced2
Svitlana-G <svitlana.golubieva@gmail.com>
2024-02-09 17:07:38 +0100

    Add commit and button

--- frontend/src/components/Order/components/PostcardAndComment/PostcardAndComment.jsx
+++ frontend/src/components/Order/components/PostcardAndComment/PostcardAndComment.jsx
@@ -1,16 +1,18 @@
 import React, { useState } from 'react';
 import { useForm } from 'react-hook-form';
 import styles from './PostcardAndComment.module.scss';
+import Button from '../../../ui-kit/components/Button';
 
 const PostcardAndComment = ({ postcardData }) => {
-  const [showPostcardTextarea, setShowPostcardTextarea] = useState(true);
+  const [showPostcardText, setShowPostcardText] = useState(true);
+  const [showComment, setShowComment] = useState(false);
   const { register } = useForm({
     defaultValues: {
       postcard: postcardData?.postcard ? postcardData.postCard : 'Add postcard',
     },
   });
   return (
-    <form>
+    <form className={styles.postcardForm}>
       <label htmlFor="postcard" className={styles.radioBtn}>
         <input
           type="radio"
@@ -18,12 +20,12 @@ const PostcardAndComment = ({ postcardData }) => {
           value="Add postcard"
           {...register('postcard')}
           onChange={() => {
-            setShowPostcardTextarea(true);
+            setShowPostcardText(true);
           }}
         />
         Додати листівку з побажанням
       </label>
-      {showPostcardTextarea && (
+      {showPostcardText && (
         <textarea
           placeholder="Напишіть ваше побажання"
           {...register('postcardText')}
@@ -36,11 +38,31 @@ const PostcardAndComment = ({ postcardData }) => {
           value="No postcard"
           {...register('postcard')}
           onChange={() => {
-            setShowPostcardTextarea(false);
+            setShowPostcardText(false);
           }}
         />
         Без листівки
       </label>
+      <div className={styles.comment}>
+        <label htmlFor="comment" onClick={() => setShowComment(true)}>
+          Коментар до замовлення
+        </label>
+        {showComment && (
+          <textarea
+            id="comment"
+            placeholder="Напишіть ваш коментар"
+            {...register('comment')}
+          />
+        )}
+      </div>
+      <div className={styles.continueButton}>
+        <Button
+          label="Продовжити"
+          variant="primary"
+          padding="padding-even"
+          type="submit"
+        />
+      </div>
     </form>
   );
 };
