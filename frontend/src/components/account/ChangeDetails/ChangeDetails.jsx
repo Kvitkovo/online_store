a10c23bf0c4871b5bc0f7aa850ffc77ed8b9c57f
Anna Balabak <annabalabak15@gmail.com>
2024-02-23 15:45:10 +0100

    displaying user data to be corrected

--- frontend/src/components/account/ChangeDetails/ChangeDetails.jsx
+++ frontend/src/components/account/ChangeDetails/ChangeDetails.jsx
@@ -1,14 +1,56 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Account from '../Account';
 import styles from './ChangeDetails.module.scss';
 import Button from '../../ui-kit/components/Button';
+import InputMask from 'react-input-mask';
+import { useNavigate } from 'react-router-dom';
+import { useSelector } from 'react-redux';
 
 const ChangeDetails = () => {
+  const userData = useSelector((state) => state.user.user);
+  const navigate = useNavigate();
+
+  const [correctUserData, setCorrectUserData] = useState({
+    firstName: userData.firstName || '',
+    lastName: userData.lastName || '',
+    surname: userData.surname || '',
+    email: userData.email || '',
+    phone: userData.phone || '',
+    birthday: userData.birthday || '',
+  });
+
+  useEffect(() => {
+    setCorrectUserData({
+      firstName: userData.firstName || '',
+      lastName: userData.lastName || '',
+      surname: userData.surname || '',
+      email: userData.email || '',
+      phone: userData.phone || '',
+      birthday: userData.birthday || '',
+    });
+  }, [userData]);
+
+  const handleChange = (e) => {
+    const { id, value } = e.target;
+    setCorrectUserData((prevUserData) => ({
+      ...prevUserData,
+      [id]: value,
+    }));
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    /* console.log('Update user details', correctUserData); */
+  };
+
+  const handleCancel = () => {
+    navigate('/account');
+  };
   return (
     <Account title="Редагування особистої інформації">
       <div className={styles.mainContainer}>
         <h2 className={styles.header}> Контактні дані</h2>
-        <form action="">
+        <form onSubmit={handleSubmit}>
           <div className={styles.formContainer}>
             <div className={styles.nameData}>
               <div className={styles.dataContainer}>
@@ -18,6 +60,8 @@ const ChangeDetails = () => {
                   className={styles.dataInput}
                   type="text"
                   placeholder=""
+                  value={correctUserData.firstName}
+                  onChange={handleChange}
                 />
               </div>
               <div className={styles.dataContainer}>
@@ -27,26 +71,34 @@ const ChangeDetails = () => {
                   className={styles.dataInput}
                   type="text"
                   placeholder=""
+                  value={correctUserData.lastName}
+                  onChange={handleChange}
                 />
               </div>
               <div className={styles.dataContainer}>
-                <label htmlFor="patronimic">По батькові</label>
+                <label htmlFor="surname">По батькові</label>
                 <input
-                  id="patronimic"
+                  id="surname"
                   className={styles.dataInput}
                   type="text"
                   placeholder=""
+                  value={correctUserData.surname}
+                  onChange={handleChange}
                 />
               </div>
             </div>
             <div className={styles.contactsData}>
               <div className={styles.dataContainer}>
                 <label htmlFor="phoneNumber">Номер телефону</label>
-                <input
-                  id="phoneNumber"
+
+                <InputMask
+                  id="phone"
                   className={styles.dataInput}
-                  type="phone"
-                  placeholder=""
+                  mask="+380 99 9999999"
+                  maskChar=""
+                  placeholder="+380 XX XXXXXXX"
+                  value={correctUserData.phone}
+                  onChange={handleChange}
                 />
               </div>
               <div className={styles.dataContainer}>
@@ -56,15 +108,19 @@ const ChangeDetails = () => {
                   className={styles.dataInput}
                   type="email"
                   placeholder=""
+                  value={correctUserData.email}
+                  onChange={handleChange}
                 />
               </div>
               <div className={styles.dataContainer}>
                 <label htmlFor="date">Дата народження</label>
                 <input
-                  id="date"
+                  id="birthday"
                   className={styles.dataInput}
                   type="date"
                   placeholder=""
+                  value={correctUserData.birthday}
+                  onChange={handleChange}
                 />
               </div>
             </div>
@@ -80,6 +136,7 @@ const ChangeDetails = () => {
               variant="no-border-hovered"
               label="Скасувати"
               padding="padding-header-even"
+              onClick={handleCancel}
             />
           </div>
         </form>
