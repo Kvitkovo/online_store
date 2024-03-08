e735e9cb5bb9a5b4a52c813ce09c7e1e1a831f3a
Andriy Gaponov <wmconstructor@gmail.com>
2024-03-08 10:31:30 +0200

    add sales-receipt.html

---
+++ backend/src/main/java/ua/kvitkovo/orders/service/OrderPrint.java
@@ -0,0 +1,13 @@
+package ua.kvitkovo.orders.service;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@RequiredArgsConstructor
+@Service
+public class OrderPrint {
+
+    private final OrderService orderService;
+}
