4fc8393b3aee58cb437b036e98c354961083d184
Alya Rydasova <halyna.rydasova@gmail.com>
2024-03-10 11:34:11 +0200

    added second screen animation

--- frontend/src/pages/About/components/RevealAnimation/RevealAnimation.jsx
+++ frontend/src/pages/About/components/RevealAnimation/RevealAnimation.jsx
@@ -1,12 +1,13 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import styles from './RevealAnimation.module.scss';
 import { motion } from 'framer-motion';
 
 const RevealAnimation = ({ data }) => {
+  const ref = useRef(null);
   const cover = { initial: { opacity: 0.7 }, animated: { opacity: 0 } };
 
   return (
-    <ul className={styles.mainImgs}>
+    <ul ref={ref} className={styles.mainImgs}>
       {data.map((img) => {
         const { name, src, transition } = img;
         const variants =
@@ -24,17 +25,21 @@ const RevealAnimation = ({ data }) => {
             initial="initial"
             whileInView="animated"
             transition={transition}
-            viewport={{ once: true }}
+            viewport={{ once: true, margin: '-50% 0px' }}
           >
             <img src={src} alt="Працівники Kvitkovo " />
             {name === 'main' && (
               <motion.div
                 variants={cover}
                 initial="initial"
-                whileInView="animated"
+                whileInView={{ opacity: [0.75, 0.5, 0.25, 0.1, 0] }}
                 className={styles.mainImgs__main_cover}
-                transition={{ durtion: 0.75, delay: 0 }}
-                viewport={{ once: true }}
+                transition={{
+                  times: [0, 0.2, 0.4, 0.9, 1],
+                  durtion: 0.75,
+                  delay: 0.25,
+                }}
+                viewport={{ once: true, margin: '-50% 0px' }}
               ></motion.div>
             )}
           </motion.li>
