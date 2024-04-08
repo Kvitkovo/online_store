cac0a3ffce7214da68de2c66de43ac7629231f2a
Svitlana-G <svitlana.golubieva@gmail.com>
2024-04-08 14:52:58 +0200

    Fix images in personal account(orders)

--- frontend/src/components/account/Orders/components/OrderItem/OrderItem.jsx
+++ frontend/src/components/account/Orders/components/OrderItem/OrderItem.jsx
@@ -2,15 +2,16 @@ import React from 'react';
 import styles from './OrderItem.module.scss';
 
 const OrderItem = ({ number, code, item, img, itemQuantity, price }) => {
+  if (item.includes('Свій букет')) {
+    img = '/images/new_bouquet.jpg';
+  } else if (!img) img = '/images/no_image.jpg';
+
   return (
     <div className={styles.gridOrder}>
       <div>{number}</div>
       <div>код {code}</div>
       <div className={styles.imageAndTitle}>
-        <img
-          src={img ? img : '/images/new_bouquet.jpg'}
-          className={styles.image}
-        />
+        <img src={img} className={styles.image} />
         {item}
       </div>
       <div>{itemQuantity} шт</div>
