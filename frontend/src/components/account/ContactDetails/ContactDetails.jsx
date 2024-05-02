70aa851ac7e16a94d84d2f1b682e89e39091f69b
Anna Balabak <annabalabak15@gmail.com>
2024-05-02 11:49:16 +0200

    added tablet version for contact details and edit contact details

--- frontend/src/components/account/ContactDetails/ContactDetails.jsx
+++ frontend/src/components/account/ContactDetails/ContactDetails.jsx
@@ -5,6 +5,7 @@ import Button from '../../ui-kit/components/Button/Button';
 import { useNavigate } from 'react-router-dom';
 import Account from '../Account';
 import { useSelector } from 'react-redux';
+import Path from '../../../pages/CardPage/components/Path';
 
 const ContactDetails = () => {
   const navigate = useNavigate();
@@ -19,45 +20,48 @@ const ContactDetails = () => {
   };
 
   return (
-    <Account title={`Вітаємо, ${userData ? userData.firstName : ''}`}>
-      <div className={styles.block}>
-        <h2 className={styles.accountTitle}> Контактні дані</h2>
-        <div className={styles.flexFieldsAndData}>
-          <div className={styles.fields}>
-            <p>Ім&apos;я</p>
-            <p>Прізвище</p>
-            <p>По батькові</p>
-            <p>Номер телефону</p>
-            <p>Ел. пошта</p>
-            <p>Дата народження</p>
-          </div>
-          {userData && (
-            <div className={styles.data} key={userData.id}>
-              <p>{userData.firstName || 'не вказано'}</p>
-              <p>{userData.lastName || 'не вказано'}</p>
-              <p>{userData.surname || 'не вказано'}</p>
-              <p>{userData.phone || 'не вказано'}</p>
-              <p>{userData.email || 'не вказано'}</p>
-              <p>{userData.birthday || 'не вказано'}</p>
+    <>
+      <Path currentPageData={{ name: 'Кабінет' }} currentPageType="section" />
+      <Account title={`Вітаємо, ${userData ? userData.firstName : ''}`}>
+        <div className={styles.block}>
+          <h2 className={styles.accountTitle}> Контактні дані</h2>
+          <div className={styles.flexFieldsAndData}>
+            <div className={styles.fields}>
+              <p>Ім&apos;я</p>
+              <p>Прізвище</p>
+              <p>По батькові</p>
+              <p>Номер телефону</p>
+              <p>Ел. пошта</p>
+              <p>Дата народження</p>
             </div>
-          )}
+            {userData && (
+              <div className={styles.data} key={userData.id}>
+                <p>{userData.firstName || 'не вказано'}</p>
+                <p>{userData.lastName || 'не вказано'}</p>
+                <p>{userData.surname || 'не вказано'}</p>
+                <p>{userData.phone || 'не вказано'}</p>
+                <p>{userData.email || 'не вказано'}</p>
+                <p>{userData.birthday || 'не вказано'}</p>
+              </div>
+            )}
+          </div>
+          <div className={styles.flexButtons}>
+            <Button
+              variant="no-border-hovered"
+              label="Редагувати"
+              icon={<ICONS.PencilIcon />}
+              onClick={navigateToEdit}
+            />
+            <Button
+              variant="no-border-hovered"
+              label="Змінити пароль"
+              icon={<ICONS.changePassword />}
+              onClick={navigateToChangePassword}
+            />
+          </div>
         </div>
-        <div className={styles.flexButtons}>
-          <Button
-            variant="no-border-hovered"
-            label="Редагувати"
-            icon={<ICONS.PencilIcon />}
-            onClick={navigateToEdit}
-          />
-          <Button
-            variant="no-border-hovered"
-            label="Змінити пароль"
-            icon={<ICONS.changePassword />}
-            onClick={navigateToChangePassword}
-          />
-        </div>
-      </div>
-    </Account>
+      </Account>
+    </>
   );
 };
 export default ContactDetails;
