787af08444d1f059beea603b96ae78460f8522fa
Anna Balabak <annabalabak15@gmail.com>
2024-05-08 17:43:38 +0200

    created header for data editing for mobile devices

--- frontend/src/components/account/ChangeDetails/ChangeDetails.jsx
+++ frontend/src/components/account/ChangeDetails/ChangeDetails.jsx
@@ -6,12 +6,20 @@ import InputMask from 'react-input-mask';
 import { useNavigate } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 import { editUserData } from '../../../services/userData/editUserData';
+import IconButton from '../../ui-kit/components/IconButton';
+import { ICONS } from '../../ui-kit/icons';
 
 const ChangeDetails = () => {
   const userData = useSelector((state) => state.user.user);
   const navigate = useNavigate();
   const dispatch = useDispatch();
+  const [isMobile, setIsMobile] = useState(window.innerWidth <= 510);
 
+  const handleResize = () => {
+    setIsMobile();
+  };
+
+  window.addEventListener('resize', handleResize);
   const [correctUserData, setCorrectUserData] = useState(userData);
 
   const handleChange = (e) => {
@@ -34,107 +42,128 @@ const ChangeDetails = () => {
     }
   };
 
+  const navigateBack = () => {
+    navigate('/account/personal-data');
+  };
+
   const handleCancel = () => {
     navigate('/account');
   };
   return (
-    <Account title="Редагування особистої інформації">
-      <div className={styles.mainContainer}>
-        <h2 className={styles.header}> Контактні дані</h2>
-        <form onSubmit={handleSubmit}>
-          <div className={styles.formContainer}>
-            <div className={styles.nameData}>
-              <div className={styles.dataContainer}>
-                <label htmlFor="firstName">Ім’я</label>
-                <input
-                  id="firstName"
-                  className={styles.dataInput}
-                  type="text"
-                  placeholder="Введіть ваше ім’я"
-                  value={correctUserData.firstName}
-                  onChange={handleChange}
-                />
-              </div>
-              <div className={styles.dataContainer}>
-                <label htmlFor="lastName">Прізвище</label>
-                <input
-                  id="lastName"
-                  className={styles.dataInput}
-                  type="text"
-                  placeholder="Введіть ваше прізвище"
-                  value={correctUserData.lastName}
-                  onChange={handleChange}
-                />
-              </div>
-              <div className={styles.dataContainer}>
-                <label htmlFor="surname">По батькові</label>
-                <input
-                  id="surname"
-                  className={styles.dataInput}
-                  type="text"
-                  placeholder="Введіть ваше по батькові"
-                  value={correctUserData.surname}
-                  onChange={handleChange}
-                />
-              </div>
+    <>
+      {isMobile ? (
+        <>
+          <div className={styles.mobileHeader}>
+            <div>
+              <IconButton
+                icon={<ICONS.ArrowLeftIcon />}
+                onClick={navigateBack}
+              />
             </div>
-            <div className={styles.contactsData}>
-              <div className={styles.dataContainer}>
-                <label htmlFor="phoneNumber">Номер телефону</label>
+            <p className={styles.accountTitle}>Редагування даних</p>
+            <IconButton icon={<ICONS.closeMobile />} />
+          </div>
+        </>
+      ) : (
+        <Account title="Редагування особистої інформації">
+          <div className={styles.mainContainer}>
+            <h2 className={styles.header}> Контактні дані</h2>
+            <form onSubmit={handleSubmit}>
+              <div className={styles.formContainer}>
+                <div className={styles.nameData}>
+                  <div className={styles.dataContainer}>
+                    <label htmlFor="firstName">Ім’я</label>
+                    <input
+                      id="firstName"
+                      className={styles.dataInput}
+                      type="text"
+                      placeholder="Введіть ваше ім’я"
+                      value={correctUserData.firstName}
+                      onChange={handleChange}
+                    />
+                  </div>
+                  <div className={styles.dataContainer}>
+                    <label htmlFor="lastName">Прізвище</label>
+                    <input
+                      id="lastName"
+                      className={styles.dataInput}
+                      type="text"
+                      placeholder="Введіть ваше прізвище"
+                      value={correctUserData.lastName}
+                      onChange={handleChange}
+                    />
+                  </div>
+                  <div className={styles.dataContainer}>
+                    <label htmlFor="surname">По батькові</label>
+                    <input
+                      id="surname"
+                      className={styles.dataInput}
+                      type="text"
+                      placeholder="Введіть ваше по батькові"
+                      value={correctUserData.surname}
+                      onChange={handleChange}
+                    />
+                  </div>
+                </div>
+                <div className={styles.contactsData}>
+                  <div className={styles.dataContainer}>
+                    <label htmlFor="phoneNumber">Номер телефону</label>
 
-                <InputMask
-                  id="phone"
-                  className={styles.dataInput}
-                  mask="+380 99 9999999"
-                  maskChar=""
-                  placeholder="+380 XX XXXXXXX"
-                  value={correctUserData.phone}
-                  onChange={handleChange}
+                    <InputMask
+                      id="phone"
+                      className={styles.dataInput}
+                      mask="+380 99 9999999"
+                      maskChar=""
+                      placeholder="+380 XX XXXXXXX"
+                      value={correctUserData.phone}
+                      onChange={handleChange}
+                    />
+                  </div>
+                  <div className={styles.dataContainer}>
+                    <label htmlFor="email">
+                      Ел. пошта <span>*</span>
+                    </label>
+                    <input
+                      id="email"
+                      className={styles.dataInput}
+                      type="email"
+                      placeholder="Введіть електрону пошту"
+                      value={correctUserData.email}
+                      onChange={handleChange}
+                    />
+                  </div>
+                  <div className={styles.dataContainer}>
+                    <label htmlFor="date">Дата народження</label>
+                    <input
+                      id="birthday"
+                      className={`${styles.dataInput} ${styles.birthday} `}
+                      type="date"
+                      value={correctUserData.birthday}
+                      onChange={handleChange}
+                    />
+                  </div>
+                </div>
+              </div>
+              <div className={styles.btnContainer}>
+                <Button
+                  variant="primary"
+                  label="Зберегти"
+                  padding="padding-sm"
+                  type="submit"
+                  onClick={handleSubmit}
+                />
+                <Button
+                  variant="no-border-hovered"
+                  label="Скасувати"
+                  padding="padding-header-even"
+                  onClick={handleCancel}
                 />
               </div>
-              <div className={styles.dataContainer}>
-                <label htmlFor="email">
-                  Ел. пошта <span>*</span>
-                </label>
-                <input
-                  id="email"
-                  className={styles.dataInput}
-                  type="email"
-                  placeholder="Введіть електрону пошту"
-                  value={correctUserData.email}
-                  onChange={handleChange}
-                />
-              </div>
-              <div className={styles.dataContainer}>
-                <label htmlFor="date">Дата народження</label>
-                <input
-                  id="birthday"
-                  className={`${styles.dataInput} ${styles.birthday} `}
-                  type="date"
-                  value={correctUserData.birthday}
-                  onChange={handleChange}
-                />
-              </div>
-            </div>
+            </form>
           </div>
-          <div className={styles.btnContainer}>
-            <Button
-              variant="primary"
-              label="Зберегти"
-              padding="padding-sm"
-              type="submit"
-              onClick={handleSubmit}
-            />
-            <Button
-              variant="no-border-hovered"
-              label="Скасувати"
-              padding="padding-header-even"
-              onClick={handleCancel}
-            />
-          </div>
-        </form>
-      </div>
-    </Account>
+        </Account>
+      )}
+    </>
   );
 };
 
