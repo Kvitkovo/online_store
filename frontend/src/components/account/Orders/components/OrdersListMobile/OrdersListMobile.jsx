e019ab74ee6ad71758025571a710e51801ae789c
Svitlana-G <svitlana.golubieva@gmail.com>
2024-06-05 17:07:16 +0200

    Add order details

--- frontend/src/components/account/Orders/components/OrdersListMobile/OrdersListMobile.jsx
+++ frontend/src/components/account/Orders/components/OrdersListMobile/OrdersListMobile.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import styles from './OrdersListMobile.module.scss';
 import OrderDeleting from '../OrderDeleteIcon/OrderDeleteIcon';
 import { statusMapping } from '../../../../../constants/statusMapping';
@@ -7,7 +8,7 @@ const OrdersListMobile = ({ data }) => {
   return (
     <div>
       {data.map((order) => (
-        <div key={order.id}>
+        <div key={order.id} className={styles.orderList}>
           <div className={styles.blockNumber}>
             <div className={styles.title}>
               <div className={styles.number}>
@@ -35,6 +36,19 @@ const OrdersListMobile = ({ data }) => {
               <div>Статус</div>
               <div>{statusMapping[order.status]}</div>
             </div>
+          </div>
+          <div className={styles.totalAmount}>
+            <div>Сума</div>
+            <div>{order.totalSum} грн</div>
+          </div>
+          <div className={styles.receiver}>
+            <div>Отримувач</div>
+            <div>{order.receiverName}</div>
+          </div>
+          <div className={styles.details}>
+            <Link to={`/account/orders/${order.id}`} className={styles.link}>
+              Детально
+            </Link>
           </div>
         </div>
       ))}
