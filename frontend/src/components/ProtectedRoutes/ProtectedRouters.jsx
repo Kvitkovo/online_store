8d3cd68bd291b9afab7bc300cd40851b72643818
Svitlana-G <svitlana.golubieva@gmail.com>
2024-06-14 15:00:33 +0200

    Fix token in protected routes

--- frontend/src/components/ProtectedRoutes/ProtectedRouters.jsx
+++ frontend/src/components/ProtectedRoutes/ProtectedRouters.jsx
@@ -2,7 +2,8 @@ import { Navigate, Outlet } from 'react-router-dom';
 import React from 'react';
 
 const ProtectedRoutes = ({ isLoggedIn, updateLoginStatus }) => {
-  let auth = { token: true };
+  let auth = { token: localStorage.getItem('authToken') };
+
   const handleAuthentication = () => {
     if (!auth.token && isLoggedIn) {
       updateLoginStatus(false);
