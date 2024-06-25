01e7ab7184a768706d59366c5da337e5064ecd4f
Svitlana-G <svitlana.golubieva@gmail.com>
2024-06-25 16:47:55 +0200

    Add a check if the token is valid

--- frontend/src/components/ProtectedRoutes/ProtectedRouters.jsx
+++ frontend/src/components/ProtectedRoutes/ProtectedRouters.jsx
@@ -1,21 +1,48 @@
-import { Navigate, Outlet } from 'react-router-dom';
-import React from 'react';
-import Account from '../account/Account';
+import { Navigate, Outlet, useNavigate } from 'react-router-dom';
+import React, { useEffect, useState } from 'react';
+import { jwtDecode } from 'jwt-decode';
+import { useDispatch } from 'react-redux';
+import { logout } from '../../redux/slices/userSlice';
 
-const ProtectedRoutes = ({ isLoggedIn, updateLoginStatus }) => {
-  let auth = { token: localStorage.getItem('authToken') };
+const ProtectedRoutes = ({ updateLoginStatus }) => {
+  const [auth, setAuth] = useState({
+    token: localStorage.getItem('authToken'),
+  });
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
 
-  const handleAuthentication = () => {
-    if (!auth.token && isLoggedIn) {
-      updateLoginStatus(false);
+  const getTokenExpirationTime = (token) => {
+    try {
+      const decodedToken = jwtDecode(token);
+      return decodedToken.exp * 1000;
+    } catch (error) {
+      console.error('Failed to decode token', error);
+      return null;
     }
   };
-  handleAuthentication();
-  return (
-    <>
-      <Account>{auth.token ? <Outlet /> : <Navigate to="/" />}</Account>
-    </>
-  );
+
+  const isTokenValid = (token) => {
+    const expirationTime = getTokenExpirationTime(token);
+    if (!expirationTime) return false;
+    const currentTime = new Date().getTime();
+    return currentTime < expirationTime;
+  };
+
+  const handleAuthentication = () => {
+    if (!auth.token || !isTokenValid(auth.token)) {
+      const itemsToRemove = ['authToken', 'authId', 'userfetchedData', 'email'];
+      itemsToRemove.forEach((item) => localStorage.removeItem(item));
+      dispatch(logout());
+      updateLoginStatus(false);
+      setAuth({ token: null });
+      navigate('/');
+    }
+  };
+  useEffect(() => {
+    handleAuthentication();
+  });
+
+  return <>{auth.token ? <Outlet /> : <Navigate to="/" />}</>;
 };
 
 export default ProtectedRoutes;
