7c3f8e99f15b1dccab1c9c4713a046d4e5f8493a
Svitlana-G <svitlana.golubieva@gmail.com>
2024-07-05 14:49:49 +0200

    Add setTimeot for logout when the token expires

--- frontend/src/components/ProtectedRoutes/ProtectedRouters.jsx
+++ frontend/src/components/ProtectedRoutes/ProtectedRouters.jsx
@@ -1,48 +1,38 @@
 import { Navigate, Outlet, useNavigate } from 'react-router-dom';
-import React, { useEffect, useState } from 'react';
+import React, { useEffect } from 'react';
 import { jwtDecode } from 'jwt-decode';
 import { useDispatch } from 'react-redux';
 import { logout } from '../../redux/slices/userSlice';
 
 const ProtectedRoutes = ({ updateLoginStatus }) => {
-  const [auth, setAuth] = useState({
-    token: localStorage.getItem('authToken'),
-  });
   const dispatch = useDispatch();
   const navigate = useNavigate();
+  const token = localStorage.getItem('authToken');
+  const itemsToRemove = ['authToken', 'authId', 'userfetchedData', 'email'];
 
-  const getTokenExpirationTime = (token) => {
-    try {
-      const decodedToken = jwtDecode(token);
-      return decodedToken.exp * 1000;
-    } catch (error) {
-      console.error('Failed to decode token', error);
-      return null;
-    }
+  const onExpire = () => {
+    dispatch(logout());
+    itemsToRemove.forEach((item) => localStorage.removeItem(item));
+    updateLoginStatus(false);
+    navigate('/');
   };
 
-  const isTokenValid = (token) => {
-    const expirationTime = getTokenExpirationTime(token);
-    if (!expirationTime) return false;
-    const currentTime = new Date().getTime();
-    return currentTime < expirationTime;
-  };
-
-  const handleAuthentication = () => {
-    if (!auth.token || !isTokenValid(auth.token)) {
-      const itemsToRemove = ['authToken', 'authId', 'userfetchedData', 'email'];
-      itemsToRemove.forEach((item) => localStorage.removeItem(item));
-      dispatch(logout());
-      updateLoginStatus(false);
-      setAuth({ token: null });
-      navigate('/');
-    }
-  };
   useEffect(() => {
-    handleAuthentication();
+    if (token) {
+      const decodedTocken = jwtDecode(token);
+      const currentTime = new Date().getTime();
+      const expiryTime = new Date(decodedTocken.exp * 1000).getTime();
+      const timeout = expiryTime - currentTime;
+
+      if (timeout > 0) {
+        setTimeout(onExpire, timeout);
+      } else {
+        onExpire();
+      }
+    }
   });
 
-  return <>{auth.token ? <Outlet /> : <Navigate to="/" />}</>;
+  return <>{token ? <Outlet /> : <Navigate to="/" />}</>;
 };
 
 export default ProtectedRoutes;
