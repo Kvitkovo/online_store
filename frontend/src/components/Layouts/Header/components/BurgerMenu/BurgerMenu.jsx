d687220b8ca56b8371a5df5206df0d70539a98dc
Andrii Valenko <102794464+vnko1@users.noreply.github.com>
2024-07-12 20:15:33 +0300

    fix: Fix scroll on burger menu open

--- frontend/src/components/Layouts/Header/components/BurgerMenu/BurgerMenu.jsx
+++ frontend/src/components/Layouts/Header/components/BurgerMenu/BurgerMenu.jsx
@@ -1,9 +1,11 @@
 import React, { useCallback, useEffect, useState } from 'react';
-import OpenMenu from './OpenMenu';
-import ClosedMenu from './ClosedMenu';
 import { useLocation } from 'react-router-dom';
 import { useDispatch } from 'react-redux';
 import { closeMenu } from '../../../../../redux/slices/catalogSlice';
+import { useWindowSize } from '../../../../../hooks/useWindowSize';
+import OpenMenu from './OpenMenu';
+import ClosedMenu from './ClosedMenu';
+import styles from './BurgerMenu.module.scss';
 
 const BurgerMenu = ({
   toggleCart,
@@ -15,6 +17,7 @@ const BurgerMenu = ({
   const [isOpen, setIsOpen] = useState(false);
   const location = useLocation();
   const dispatch = useDispatch();
+  const screen = useWindowSize();
 
   const toggleMenu = useCallback(() => {
     setIsOpen((prevIsOpen) => !prevIsOpen);
@@ -24,6 +27,16 @@ const BurgerMenu = ({
   useEffect(() => {
     setIsOpen(false);
   }, [location.pathname]);
+
+  useEffect(() => {
+    isOpen && document.body.classList.add(styles.disableScroll);
+    if (screen.width > 868)
+      document.body.classList.remove(styles.disableScroll);
+    return () => {
+      document.body.classList.remove(styles.disableScroll);
+    };
+  }, [isOpen, screen.width]);
+
   return (
     <>
       {isOpen ? (
