f00fbde63d1c515fc25f5ca7e37851b30d42224a
Andrii Valenko <andreyvalenko@gmail.com>
2024-08-06 13:30:51 +0300

    feat: add animation

--- frontend/src/pages/Partner/components/Advantages/Advantages.jsx
+++ frontend/src/pages/Partner/components/Advantages/Advantages.jsx
@@ -1,12 +1,57 @@
-import React from 'react';
-// import { motion } from 'framer-motion';
-import styles from './Advantages.module.scss';
+import React, { useState } from 'react';
+import { motion, AnimatePresence } from 'framer-motion';
 
-// import data from '../../../../data/partnerData.json';
+import { ICONS } from '../../../../components/ui-kit/icons';
+import styles from './Advantages.module.scss';
+import data from '../../../../data/partnerData.json';
 
 const AdvantagesAnimation = () => {
-  // console.log('🚀 ~ data:', data.advantages);
-  return <div className={styles}>AdvantagesAnimation</div>;
+  const [rotation, setRotation] = useState(0);
+  const [selectedData, setSelectedData] = useState(data[0]);
+
+  const handleRotate = (id) => {
+    setRotation((state) => (state += 90));
+    setSelectedData(data[id]);
+  };
+  return (
+    <div className={styles.wrapper}>
+      <motion.div
+        className={styles.box}
+        animate={{ rotate: rotation }}
+        transition={{ ease: 'easeInOut', duration: 0.5 }}
+      >
+        {data.map(({ icon, classNames }, index) => {
+          const IconComponent = ICONS[icon];
+          return (
+            <button
+              key={index}
+              className={`${styles.button} ${styles[classNames]}`}
+              onClick={() => handleRotate(index)}
+            >
+              <IconComponent />
+            </button>
+          );
+        })}
+      </motion.div>
+      <AnimatePresence mode="wait">
+        <motion.div
+          key={selectedData ? selectedData.title : 'empty'}
+          initial={{ y: 10, opacity: 0 }}
+          animate={{ y: 0, opacity: 1 }}
+          exit={{ y: -10, opacity: 0 }}
+          transition={{ duration: 0.2 }}
+          className={styles.content}
+        >
+          {selectedData ? (
+            <div className={styles.text}>
+              <h3 className={styles.title}>{selectedData.title}</h3>
+              <p>{selectedData.description}</p>
+            </div>
+          ) : null}
+        </motion.div>
+      </AnimatePresence>
+    </div>
+  );
 };
 
 export default AdvantagesAnimation;
